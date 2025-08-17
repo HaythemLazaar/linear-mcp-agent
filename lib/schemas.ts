ba1b3e99230d54fa651fe523aff00f1642ec08e6
HaythemLazaar <haythem.lazaar1@gmail.com>
2025-08-17 22:37:20 +0100

    chore: improve some schemas

--- lib/schemas.ts
+++ lib/schemas.ts
@@ -3,8 +3,8 @@ import z from "zod";
 const teamSchema = z.object({
   id: z.string(),
   name: z.string(),
-  createdAt: z.string(),
-  updatedAt: z.string(),
+  createdAt: z.string().datetime(),
+  updatedAt: z.string().datetime(),
 });
 
 const teamsSchema = z.object({
@@ -14,9 +14,9 @@ const teamsSchema = z.object({
 const projectSchema = z.object({
   id: z.string(),
   name: z.string(),
-  createdAt: z.string(),
-  updatedAt: z.string(),
-  url: z.string().optional(),
+  createdAt: z.string().datetime(),
+  updatedAt: z.string().datetime(),
+  url: z.string().url().optional(),
   description: z.string().optional(),
   summary: z.string().optional(),
   startDate: z.string().optional(),
